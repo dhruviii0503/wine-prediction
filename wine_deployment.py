87f3c068507e7ed23f239d8aec835b192e81094d
Dhruvi Parmar <dhruviii0503@gmail.com>
2026-02-22 11:15:36 +0530

    Update wine_deployment.py

--- wine_deployment.py
+++ wine_deployment.py
@@ -8,16 +8,18 @@ Original file is located at
 """
 
 import streamlit as st
+import pandas as pd
 import joblib
 
-model = joblib.load("dtc_model.ppkl")
+
+model = joblib.load("dtc_model.pkl")
 
 st.title("wine type prediction !")
 
 fixed_acidity= st.number_input("fixed_acidity")
 volatile_acidity= st.number_input("volatile_acidity")
 citric_acid= st.number_input("citric_acid")
-residual_sugar= st.number_input("residual_suga")
+residual_sugar= st.number_input("residual_sugar")
 chlorides= st.number_input("chlorides")
 free_sulfur_dioxide= st.number_input("free_sulfur_dioxide")
 total_sulfur_dioxide= st.number_input("total_sulfur_dioxide")
